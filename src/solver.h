0c7297aa1e8ac31897e63bcb7c9ab565c9b330db
spinute <spinutids@gmail.com>
2016-09-18 18:02:00 +0900

    refactoring

--- src/solver.h
+++ src/solver.h
@@ -2,8 +2,7 @@
 
 #include "state.h"
 
-void solver_dfs(State init_state,
-                State goal_state); /* have no guarantee to finish since no
-                                      duplicate detection, for now */
+void solver_dfs(State init_state, State goal_state);
 void solver_stack_dfs(State init_state, State goal_state);
 void solver_bfs(State init_state, State goal_state);
+
