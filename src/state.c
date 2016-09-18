6a3e7734cb6630c434885d94db391dfe81407d55
spinute <spinutids@gmail.com>
2016-09-18 22:14:25 +0900

    merge heuristic into state

--- src/state.c
+++ src/state.c
@@ -92,7 +92,6 @@ State
 state_copy(State src)
 {
     State dst = state_alloc();
-    ;
 
     memcpy(dst, src, sizeof(*src));
 
@@ -194,4 +193,73 @@ state_dump(State state)
         elog("\n");
     }
     elog("-----------\n");
+}
+
+/*
+ * Heuristic functions
+ */
+
+int
+heuristic_manhattan_distance(State from, State to)
+{
+	/* TODO: implement */
+	(void) from;
+	(void) to;
+	return 0;
+}
+int
+heuristic_linear_conflict(State from, State to)
+{
+	/* TODO: implement */
+	(void) from;
+	(void) to;
+	return 0;
+}
+int
+heuristic_pattern_database(State from, State to)
+{
+	/* TODO: implement */
+	(void) from;
+	(void) to;
+	return 0;
+}
+int
+heuristic_misplaced_tiles(State from, State to)
+{
+	/* TODO: implement */
+	(void) from;
+	(void) to;
+	return 0;
+}
+int
+heuristic_nillson(State from, State to)
+{
+	/* TODO: implement */
+	(void) from;
+	(void) to;
+	return 0;
+}
+int
+heuristic_n_max_swap(State from, State to)
+{
+	/* TODO: implement */
+	(void) from;
+	(void) to;
+	return 0;
+}
+int
+heuristic_xy(State from, State to)
+{
+	/* TODO: implement */
+	(void) from;
+	(void) to;
+	return 0;
+}
+int
+heuristic_tiles_out_of_row_col(State from, State to)
+{
+	/* TODO: implement */
+	(void) from;
+	(void) to;
+	return 0;
 }
--- src/state.h
+++ src/state.h
@@ -29,8 +29,17 @@ void state_fini(State state);
 State state_copy(State src);
 
 int state_get_depth(State state);
-
 bool state_movable(State state, Direction dir);
 void state_move(State state, Direction dir);
 bool state_pos_equal(State s1, State s2);
 void state_dump(State state);
+
+// Heuristics below are found at https://heuristicswiki.wikispaces.com/N+-+Puzzle
+int heuristic_manhattan_distance(State from, State to);
+int heuristic_linear_conflict(State from, State to);
+int heuristic_pattern_database(State from, State to);
+int heuristic_misplaced_tiles(State from, State to);
+int heuristic_nillson(State from, State to);
+int heuristic_n_max_swap(State from, State to);
+int heuristic_xy(State from, State to);
+int heuristic_tiles_out_of_row_col(State from, State to);
