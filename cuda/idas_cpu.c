93266850e2878ca965635cf354c54ca5aef1e4e0
spinute <spinutids@gmail.com>
2016-11-06 22:38:05 +0900

    optimize state_move

--- cuda/idas_cpu.c
+++ cuda/idas_cpu.c
@@ -30,14 +30,14 @@ static struct dir_stack_tag
 	uchar buf[STACK_BUF_BYTES];
 } stack;
 
-	static inline void
+static inline void
 stack_put(Direction dir)
 {
 	stack_byte(stack.i) &= ~(STACK_DIR_MASK << stack_ofs(stack.i));
 	stack_byte(stack.i) |= dir << stack_ofs(stack.i);
 	++stack.i;
 }
-	static inline bool
+static inline bool
 stack_is_empty(void)
 {
 	return stack.i == 0;
@@ -90,6 +90,32 @@ static uchar inline distance(uchar i, uchar j)
 	return i > j ? i - j : j - i;
 }
 
+#define H_DIFF(who, from_x, from_y, dir) h_diff_table[who][from_x][from_y][dir]
+static int h_diff_table[STATE_N][STATE_WIDTH][STATE_WIDTH][N_DIR];
+
+static void init_mdist(void)
+{
+	for (int who = 0; who < STATE_N; ++who)
+	{
+		int goal_x = who % STATE_WIDTH,
+			goal_y = who / STATE_WIDTH;
+
+		for (int from_x = 0; from_x < STATE_WIDTH; ++from_x)
+			for (int from_y = 0; from_y < STATE_WIDTH; ++from_y)
+				for (uchar dir = 0; dir < N_DIR; ++dir)
+				{
+					if (dir == DIR_RIGHT)
+						H_DIFF(who, from_x, from_y, dir) = goal_x > from_x ? -1 : 1;
+					if (dir == DIR_LEFT)
+						H_DIFF(who, from_x, from_y, dir) = goal_x < from_x ? -1 : 1;
+					if (dir == DIR_DOWN)
+						H_DIFF(who, from_x, from_y, dir) = goal_y > from_y ? -1 : 1;
+					if (dir == DIR_UP)
+						H_DIFF(who, from_x, from_y, dir) = goal_y < from_y ? -1 : 1;
+				}
+	}
+}
+
 	static inline void
 state_init_hvalue(void)
 {
@@ -105,7 +131,7 @@ state_init_hvalue(void)
 			from_y[state_tile_get(x, y)] = y;
 		}
 
-	for (i = 1; i < STATE_WIDTH * STATE_WIDTH; ++i)
+	for (i = 1; i < STATE_N; ++i)
 	{
 		state.h_value += distance(from_x[i], i % STATE_WIDTH);
 		state.h_value += distance(from_y[i], i / STATE_WIDTH);
@@ -184,80 +210,41 @@ state_dump(void)
 	printf("-----------\n");
 }
 
-#define h_diff(dir)                                                            \
-	(h_diff_table[(state_tile_get(state.i, state.j) << 6) + ((state.j) << 4) + \
-	 ((state.i) << 2) + (dir)])
-const static int h_diff_table[STATE_N * STATE_N * N_DIR] = {
-	1,  1,  1,  1,  1,  1,  -1, 1,  1,  1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  1,
-	1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  1,  -1, 1,
-	-1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  -1,
-	1,  -1, 1,  -1, 1,  -1, 1,  1,  -1, 1,  1,  1,  1,  1,  1,  1,  1,  -1, 1,
-	1,  1,  -1, 1,  -1, -1, 1,  1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  -1, 1,  -1,
-	1,  -1, -1, 1,  1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, -1,
-	1,  1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  -1, 1,  1,  1,
-	-1, 1,  1,  1,  1,  1,  1,  1,  1,  -1, 1,  -1, -1, 1,  1,  -1, -1, 1,  1,
-	-1, 1,  1,  1,  -1, 1,  -1, 1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, 1,  1,
-	1,  -1, 1,  -1, 1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, 1,  1,  1,  -1, 1,
-	-1, 1,  1,  -1, 1,  1,  1,  -1, 1,  1,  1,  -1, 1,  1,  1,  1,  1,  1,  -1,
-	-1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, 1,  1,  1,  -1, -1, 1,  1,
-	-1, -1, 1,  1,  -1, -1, 1,  1,  -1, 1,  1,  1,  -1, -1, 1,  1,  -1, -1, 1,
-	1,  -1, -1, 1,  1,  -1, 1,  1,  1,  1,  1,  1,  -1, 1,  1,  -1, -1, 1,  1,
-	-1, -1, 1,  1,  -1, -1, 1,  1,  1,  1,  1,  1,  -1, 1,  1,  1,  -1, 1,  1,
-	1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,
-	-1, 1,  1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  -1, 1,
-	-1, 1,  1,  1,  -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  -1, 1,  1,  1,  1,
-	1,  1,  1,  1,  -1, 1,  1,  1,  -1, 1,  -1, -1, 1,  1,  -1, 1,  1,  1,  -1,
-	1,  -1, 1,  -1, 1,  -1, 1,  -1, -1, 1,  1,  -1, 1,  1,  1,  -1, 1,  -1, 1,
-	-1, 1,  -1, 1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  1,  -1,
-	-1, 1,  -1, 1,  1,  1,  -1, 1,  1,  1,  1,  1,  1,  1,  1,  -1, 1,  -1, -1,
-	1,  1,  -1, -1, 1,  1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  -1, -1, 1,  1,  -1,
-	-1, 1,  1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  1,  -1, 1,  -1, 1,  -1, 1,  -1,
-	1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  1,  1,  -1, 1,
-	1,  1,  1,  1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, 1,
-	1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, 1,  1,  1,  1,
-	1,  1,  -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  1,  -1,
-	1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  1,  1,  1,  1,  -1,
-	1,  1,  1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  -1, 1,
-	-1, 1,  -1, 1,  -1, 1,  1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  1,  -1, -1, 1,
-	1,  -1, -1, 1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  1,  -1, -1, 1,  1,  -1, -1,
-	1,  -1, 1,  1,  1,  1,  1,  1,  1,  1,  -1, 1,  1,  1,  -1, 1,  -1, -1, 1,
-	1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  -1, 1,  -1, 1,  -1,
-	1,  -1, 1,  1,  1,  -1, 1,  1,  -1, -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,
-	1,  1,  -1, 1,  1,  -1, -1, 1,  -1, 1,  1,  1,  -1, 1,  1,  1,  1,  1,  1,
-	1,  1,  -1, 1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, 1,  1,  1,  -1, 1,  -1,
-	1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  -1,
-	1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  1,  1,
-	-1, 1,  1,  1,  -1, 1,  1,  1,  1,  1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,
-	-1, -1, 1,  1,  -1, 1,  1,  1,  1,  1,  1,  -1, 1,  1,  -1, -1, 1,  1,  -1,
-	-1, 1,  1,  -1, -1, 1,  1,  1,  -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,
-	-1, -1, 1,  1,  1,  -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,
-	1,  1,  1,  1,  1,  -1, 1,  1,  1,  -1, 1,  1,  1,  -1, 1,  1,  -1, 1,  -1,
-	1,  1,  1,  -1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  -1, 1,  -1, 1,  1,  1,
-	-1, 1,  1,  -1, -1, 1,  1,  -1, -1, 1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  1,
-	-1, -1, 1,  1,  -1, -1, 1,  -1, 1,  1,  1,  1,  1,  1,  1,  1,  -1, 1,  1,
-	1,  -1, 1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  1,  -1, -1,
-	1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  1,  -1, -1, 1,  -1, 1,
-	-1, 1,  -1, 1,  -1, 1,  1,  1,  -1, 1,  1,  -1, -1, 1,  -1, 1,  1,  1,  -1,
-	1,  1,  1,  1,  1,  1,  1,  1,  -1, 1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,
-	-1, 1,  -1, 1,  1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1,
-	1,  1,  1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  -1, 1,  1,  1,
-	-1, 1,  -1, 1,  1,  1,  -1, 1,  1,  1,  -1, 1,  1,  1,  1,  1,  1};
-
 static char assert_direction
 [DIR_UP == 0 && DIR_RIGHT == 1 && DIR_LEFT == 2 && DIR_DOWN == 3 ? 1 : -1];
-	static void
+#define get_new_i(state, dir) (state.i + (dir & 1u) - ((dir & 2u) >> 1))
+#define get_new_j(state, dir) (state.j + (dir & 1u) + ((dir & 2u) >> 1) - 1)
+
+	static inline bool
+state_move_with_limit(Direction dir, unsigned int f_limit)
+{
+	int new_i = get_new_i(state, dir),
+		new_j = get_new_j(state, dir);
+	int who = state_tile_get(new_i, new_j);
+	int new_h_value = state.h_value + H_DIFF(who, new_i, new_j, dir_reverse(dir));
+
+	if (stack.i + 1 + new_h_value > f_limit)
+		return false;
+
+	state.h_value = new_h_value;
+	state_tile_set(state.i, state.j, who);
+	state.i = new_i;
+	state.j = new_j;
+
+	return true;
+}
+
+	static inline void
 state_move(Direction dir)
 {
-	int i_diff = (dir & 1u) - ((dir & 2u) >> 1),
-		j_diff = (dir & 1u) + ((dir & 2u) >> 1) - 1;
+	int new_i = get_new_i(state, dir),
+		new_j = get_new_j(state, dir);
+	int who = state_tile_get(new_i, new_j);
 
-	state_tile_set(state.i, state.j,
-			state_tile_get(state.i + i_diff, state.j + j_diff));
-
-	state.i += i_diff;
-	state.j += j_diff;
-
-	state.h_value += h_diff(dir_reverse(dir));
+	state.h_value += H_DIFF(who, new_i, new_j, dir_reverse(dir));
+	state_tile_set(state.i, state.j, who);
+	state.i = new_i;
+	state.j = new_j;
 }
 
 /*
@@ -280,13 +267,9 @@ idas_internal(unsigned int f_limit)
 		if ((stack_is_empty() || stack_peak() != dir_reverse(dir)) &&
 				state_movable((Direction) dir))
 		{
-			state_move((Direction) dir);
-
-			if (stack.i + state.h_value > f_limit)
-				state_move(dir_reverse(dir));
-			else
+			if (state_move_with_limit(dir, f_limit))
 			{
-				stack_put((Direction) dir);
+				stack_put(dir);
 				dir = 0;
 				continue;
 			}
@@ -306,14 +289,16 @@ idas_internal(unsigned int f_limit)
 	void
 idas_kernel(uchar *input)
 {
-    unsigned int f_limit;
-    state_tile_fill(input);
-    state_init_hvalue();
-    state_dump();
+	unsigned int f_limit;
+	state_tile_fill(input);
+	state_init_hvalue();
+	init_mdist();
 
-    for (f_limit = state.h_value;; ++f_limit)
-        if (idas_internal(f_limit))
-            break;
+	state_dump();
+
+	for (f_limit = state.h_value;; ++f_limit)
+		if (idas_internal(f_limit))
+			break;
 }
 
 /* host implementation */
@@ -323,75 +308,75 @@ idas_kernel(uchar *input)
 #include <stdlib.h>
 
 #define exit_failure(...)                                                      \
-    do                                                                         \
-    {                                                                          \
-        printf(__VA_ARGS__);                                                   \
-        exit(EXIT_FAILURE);                                                    \
-    } while (0)
+	do                                                                         \
+{                                                                          \
+	printf(__VA_ARGS__);                                                   \
+	exit(EXIT_FAILURE);                                                    \
+} while (0)
 
-static int
+	static int
 pop_int_from_str(const char *str, char **end_ptr)
 {
-    long int rv = strtol(str, end_ptr, 0);
-    errno       = 0;
+	long int rv = strtol(str, end_ptr, 0);
+	errno       = 0;
 
-    if (errno != 0)
-        exit_failure("%s: %s cannot be converted into long\n", __func__, str);
-    else if (end_ptr && str == *end_ptr)
-        exit_failure("%s: reach end of string", __func__);
+	if (errno != 0)
+		exit_failure("%s: %s cannot be converted into long\n", __func__, str);
+	else if (end_ptr && str == *end_ptr)
+		exit_failure("%s: reach end of string", __func__);
 
-    return (int) rv;
+	return (int) rv;
 }
 
 #define MAX_LINE_LEN 100
-static void
+	static void
 load_state_from_file(const char *fname, uchar *s)
 {
-    FILE *fp;
-    char  str[MAX_LINE_LEN];
-    char *str_ptr = str, *end_ptr;
-    int   i;
+	FILE *fp;
+	char  str[MAX_LINE_LEN];
+	char *str_ptr = str, *end_ptr;
+	int   i;
 
-    fp = fopen(fname, "r");
-    if (!fp)
-        exit_failure("%s: %s cannot be opened\n", __func__, fname);
+	fp = fopen(fname, "r");
+	if (!fp)
+		exit_failure("%s: %s cannot be opened\n", __func__, fname);
 
-    if (!fgets(str, MAX_LINE_LEN, fp))
-        exit_failure("%s: fgets failed\n", __func__);
+	if (!fgets(str, MAX_LINE_LEN, fp))
+		exit_failure("%s: fgets failed\n", __func__);
 
-    for (i = 0; i < STATE_N; ++i)
-    {
-        s[i]    = pop_int_from_str(str_ptr, &end_ptr);
-        str_ptr = end_ptr;
-    }
+	for (i = 0; i < STATE_N; ++i)
+	{
+		s[i]    = pop_int_from_str(str_ptr, &end_ptr);
+		str_ptr = end_ptr;
+	}
 
-    fclose(fp);
+	fclose(fp);
 }
 #undef MAX_LINE_LEN
 
 #define CUDA_CHECK(call)                                                       \
-    do                                                                         \
-    {                                                                          \
-        const cudaError_t e = call;                                            \
-        if (e != cudaSuccess)                                                  \
-            exit_failure("Error: %s:%d code:%d, reason: %s\n", __FILE__,       \
-                         __LINE__, e, cudaGetErrorString(e));                  \
-    } while (0)
+	do                                                                         \
+{                                                                          \
+	const cudaError_t e = call;                                            \
+	if (e != cudaSuccess)                                                  \
+	exit_failure("Error: %s:%d code:%d, reason: %s\n", __FILE__,       \
+			__LINE__, e, cudaGetErrorString(e));                  \
+} while (0)
 
-int
+	int
 main(int argc, char *argv[])
 {
-    uchar s_list[STATE_N];
-    int   plan[PLAN_LEN_MAX];
+	uchar s_list[STATE_N];
+	int   plan[PLAN_LEN_MAX];
 
-    if (argc < 2)
-    {
-        printf("usage: bin/cumain <ifname>\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc < 2)
+	{
+		printf("usage: bin/cumain <ifname>\n");
+		exit(EXIT_FAILURE);
+	}
 
-    load_state_from_file(argv[1], s_list);
-    idas_kernel(s_list);
+	load_state_from_file(argv[1], s_list);
+	idas_kernel(s_list);
 
-    return 0;
+	return 0;
 }
