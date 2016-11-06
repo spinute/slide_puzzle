c9b621dec538c048d238fcd3cddf8738f04ed982
spinute <spinutids@gmail.com>
2016-11-07 00:32:10 +0900

    snapshot

--- script/run.rb
+++ script/run.rb
@@ -4,7 +4,8 @@ require 'benchmark'
 
 100.times do |i|
 	puts "prob%03d" % i
+	$stderr.print i%10==0 ? '*' : '.'
 	puts Benchmark.measure {
-		`bin/main -i benchmarks/korf/prob#{"%03d" % i} -s 10`
+		`./cpumain benchmarks/korf/prob#{"%03d" % i}`
 	}
 end
