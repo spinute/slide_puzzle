a45e57c3bb3ba62c30972a5c30c9dcdf71c3eb8e
spinute <spinutids@gmail.com>
2016-11-08 16:02:54 +0000

    use individual stack/state

--- src/idas_comm_test.cu
+++ src/idas_comm_test.cu
@@ -3,6 +3,7 @@
 
 #define N_CORE 48*32
 #define N_BLOCK 48
+#define WARP_SIZE 32
 
 typedef unsigned char uchar;
 
@@ -27,35 +28,35 @@ __device__ __shared__ static struct dir_stack_tag
 {
     uchar i;
     uchar buf[STACK_BUF_BYTES];
-} stack;
+} stack[WARP_SIZE];
 
-#define stack_byte(i) (stack.buf[(i) >> STACK_DIR_BITS])
+#define stack_byte(i) (stack[threadIdx.x].buf[(i) >> STACK_DIR_BITS])
 #define stack_ofs(i) ((i & STACK_DIR_MASK) << 1)
 #define stack_get(i)                                                           \
     ((stack_byte(i) & (STACK_DIR_MASK << stack_ofs(i))) >> stack_ofs(i))
 __device__ static inline void
 stack_put(Direction dir)
 {
-    stack_byte(stack.i) &= ~(STACK_DIR_MASK << stack_ofs(stack.i));
-    stack_byte(stack.i) |= dir << stack_ofs(stack.i);
-    ++stack.i;
+    stack_byte(stack[threadIdx.x].i) &= ~(STACK_DIR_MASK << stack_ofs(stack[threadIdx.x].i));
+    stack_byte(stack[threadIdx.x].i) |= dir << stack_ofs(stack[threadIdx.x].i);
+    ++stack[threadIdx.x].i;
 }
 __device__ static inline bool
 stack_is_empty(void)
 {
-    return stack.i == 0;
-    /* how about !stack.i */
+    return stack[threadIdx.x].i == 0;
+    /* how about !stack[threadIdx.x].i */
 }
 __device__ static inline Direction
 stack_pop(void)
 {
-    --stack.i;
-    return stack_get(stack.i);
+    --stack[threadIdx.x].i;
+    return stack_get(stack[threadIdx.x].i);
 }
 __device__ static inline Direction
 stack_peak(void)
 {
-    return stack_get(stack.i - 1);
+    return stack_get(stack[threadIdx.x].i - 1);
 }
 
 /* state implementation */
@@ -78,16 +79,16 @@ __device__ __shared__ static struct state_tag
     unsigned long long tile; /* packed representation label(4bit)*16pos */
     uchar              empty;
     uchar              h_value; /* ub of h_value is 6*16 */
-} state;
+} state[WARP_SIZE];
 
 #define state_tile_ofs(i) (i << 2)
 #define state_tile_get(i)                                                      \
-    ((state.tile & (STATE_TILE_MASK << state_tile_ofs(i))) >> state_tile_ofs(i))
+    ((state[threadIdx.x].tile & (STATE_TILE_MASK << state_tile_ofs(i))) >> state_tile_ofs(i))
 #define state_tile_set(i, val)                                                 \
     do                                                                         \
     {                                                                          \
-        state.tile &= ~((STATE_TILE_MASK) << state_tile_ofs(i));               \
-        state.tile |= ((unsigned long long) val) << state_tile_ofs(i);         \
+        state[threadIdx.x].tile &= ~((STATE_TILE_MASK) << state_tile_ofs(i));               \
+        state[threadIdx.x].tile |= ((unsigned long long) val) << state_tile_ofs(i);         \
     } while (0)
 
 __device__ static uchar inline distance(uchar i, uchar j)
@@ -112,8 +113,8 @@ state_init_hvalue(void)
 	}
 	for (int i = 1; i < STATE_N; ++i)
 	{
-		state.h_value += distance(from_x[i], POS_X(i));
-		state.h_value += distance(from_y[i], POS_Y(i));
+		state[threadIdx.x].h_value += distance(from_x[i], POS_X(i));
+		state[threadIdx.x].h_value += distance(from_y[i], POS_Y(i));
 	}
 }
 
@@ -123,7 +124,7 @@ state_tile_fill(const uchar v_list[STATE_WIDTH * STATE_WIDTH])
 	for (int i = 0; i < STATE_N; ++i)
 	{
 		if (v_list[i] == STATE_EMPTY)
-			state.empty = i;
+			state[threadIdx.x].empty = i;
 		state_tile_set(i, v_list[i]);
 	}
 }
@@ -131,7 +132,7 @@ state_tile_fill(const uchar v_list[STATE_WIDTH * STATE_WIDTH])
 	__device__ static inline bool
 state_is_goal(void)
 {
-	return state.h_value == 0;
+	return state[threadIdx.x].h_value == 0;
 }
 
 __device__ static char assert_direction2
@@ -141,7 +142,7 @@ __device__ __shared__ static bool movable_table_shared[STATE_N][DIR_N];
 	__device__ static inline bool
 state_movable(Direction dir)
 {
-	return movable_table_shared[state.empty][dir];
+	return movable_table_shared[state[threadIdx.x].empty][dir];
 }
 
 __device__ static char assert_direction
@@ -152,16 +153,16 @@ __device__ __constant__ const static int pos_diff_table[DIR_N] = {-STATE_WIDTH, 
 	__device__ static inline bool
 state_move_with_limit(Direction dir, unsigned int f_limit)
 {
-	int new_empty   = state.empty + pos_diff_table[dir];
+	int new_empty   = state[threadIdx.x].empty + pos_diff_table[dir];
 	int opponent    = state_tile_get(new_empty);
-	int new_h_value = state.h_value + H_DIFF(opponent, new_empty, dir);
+	int new_h_value = state[threadIdx.x].h_value + H_DIFF(opponent, new_empty, dir);
 
-	if (stack.i + 1 + new_h_value > f_limit)
+	if (stack[threadIdx.x].i + 1 + new_h_value > f_limit)
 		return false;
 
-	state.h_value = new_h_value;
-	state_tile_set(state.empty, opponent);
-	state.empty = new_empty;
+	state[threadIdx.x].h_value = new_h_value;
+	state_tile_set(state[threadIdx.x].empty, opponent);
+	state[threadIdx.x].empty = new_empty;
 
 	return true;
 }
@@ -169,12 +170,12 @@ state_move_with_limit(Direction dir, unsigned int f_limit)
 	__device__ static inline void
 state_move(Direction dir)
 {
-	int new_empty = state.empty + pos_diff_table[dir];
+	int new_empty = state[threadIdx.x].empty + pos_diff_table[dir];
 	int opponent  = state_tile_get(new_empty);
 
-	state.h_value += H_DIFF(opponent, new_empty, dir);
-	state_tile_set(state.empty, opponent);
-	state.empty = new_empty;
+	state[threadIdx.x].h_value += H_DIFF(opponent, new_empty, dir);
+	state_tile_set(state[threadIdx.x].empty, opponent);
+	state[threadIdx.x].empty = new_empty;
 }
 
 /*
@@ -214,7 +215,6 @@ idas_internal(uchar f_limit)
 }
 
 #define NOT_SOLVED -1
-#define WARP_SIZE 32
 	__global__ void
 idas_kernel(uchar *input, char *plan, int f_limit, signed char *h_diff_table, bool *movable_table)
 {
@@ -246,8 +246,8 @@ idas_kernel(uchar *input, char *plan, int f_limit, signed char *h_diff_table, bo
 
 	solved = idas_internal(f_limit);
 
-	plan[t_ofs] = solved ? (int) stack.i : NOT_SOLVED; /* len of plan */
-	for (uchar i = 0; i < stack.i; ++i)
+	plan[t_ofs] = solved ? (int) stack[threadIdx.x].i : NOT_SOLVED; /* len of plan */
+	for (uchar i = 0; i < stack[threadIdx.x].i; ++i)
 		plan[i + 1 + t_ofs] = stack_get(i);
 }
 
