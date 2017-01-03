a95da2825d1625249b5d98a8cf1b3cb27d0447a4
spinute <spinutids@gmail.com>
2017-01-03 07:54:09 +0000

    fix script

--- script/run.rb
+++ script/run.rb
@@ -5,12 +5,11 @@ require 'timeout'
 
 100.times do |i|
 	puts "prob%03d" % i
-	$stderr.print i%10==0 ? '*' : '.'
 		puts Benchmark.measure {
 			#puts `bin/cpumain benchmarks/all/prob#{"%03d" % i}`
-			#puts `bin/cusingle benchmarks/all/prob#{"%03d" % i}`
+			puts `bin/cusingle benchmarks/all/prob#{"%03d" % i}`
 			#puts `bin/cumain benchmarks/all/prob#{"%03d" % i}`
-			puts `bin/cumem benchmarks/all/prob#{"%03d" % i}`
+			#puts `bin/cumem benchmarks/all/prob#{"%03d" % i}`
 			#puts `bin/15md_solver idastar < benchmarks/burns/prob#{"%03d" % i}`
 		}
 end
